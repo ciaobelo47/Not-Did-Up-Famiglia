302fb6f3ffc8db90ca300b49b1ed2845cd5b43df
ciaobelo47 <milodipuorto@gmail.com>
2024-05-23 18:30:17 +0200

    Second Commit

--- scripts/dropdownHandler.js
+++ scripts/dropdownHandler.js
@@ -1,18 +1,45 @@
-window.onload = () => {
-    downBTN = document.getElementById('dropdownBTN')
-    downMenuAlunno = document.getElementById('dropdown-content')
+var liAlunno = document.getElementById('Servizi Alunno');
+var liClasse = document.getElementById('Servizi Classe');
+var downAlunno = liAlunno.children[1];
+var downClasse = liClasse.children[1];
 
-    downBTN.addEventListener('click', function() {
-        if (downBTN.textContent === 'Lollo') {
-            if (downMenuAlunno.classList.contains('show')) {
-                downMenuAlunno.classList.add('hidden')
-                downMenuAlunno.classList.remove('show')
 
-            } else if (downMenuAlunno.classList.contains('hidden')) {
-                downMenuAlunno.classList.add('show')
-                downMenuAlunno.classList.remove('hidden')
-                
-            }
+liAlunno.children[0].addEventListener('click', () => {
+    if (downAlunno.classList.contains('show')) {
+        downAlunno.classList.add('hidden');
+        downAlunno.classList.remove('show');
+
+    } else if (downAlunno.classList.contains('hidden')) {
+        if (downClasse.classList.contains('show')) {
+            downClasse.classList.add('hidden');
+            downClasse.classList.remove('show');
+            downAlunno.classList.remove('hidden');
+            downAlunno.classList.add('show');
+
+        } else {
+            downAlunno.classList.remove('hidden');
+            downAlunno.classList.add('show');
+
         }
-    })
-}
+    }
+});
+
+liClasse.children[0].addEventListener('click', () => {
+    if (downClasse.classList.contains('show')) {
+        downClasse.classList.add('hidden');
+        downClasse.classList.remove('show');
+
+    } else if (downClasse.classList.contains('hidden')) {
+        if (downAlunno.classList.contains('show')) {
+            downAlunno.classList.add('hidden');
+            downAlunno.classList.remove('show');
+            downClasse.classList.remove('hidden');
+            downClasse.classList.add('show');
+
+        } else {
+            downClasse.classList.remove('hidden');
+            downClasse.classList.add('show');
+
+        }
+    }
+});
--- scripts/loading.js
+++ scripts/loading.js
@@ -1,6 +1,22 @@
+var i = 0;
+
 window.onload = () => {
-    loadingDiv = document.getElementById('loadingDiv')
-    setTimeout(() => {
-        loadingDiv.classList.add('hidden')
-    }, 3000);
+    if (i == 0) {
+        i = 1;
+        var loadingDiv = document.getElementById('loadingDiv');
+        var loadingBar = document.getElementById('loadingBar');
+        var width = 10;
+        var id = setInterval(frame, 25);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                loadingDiv.classList.add('hidden');
+                i = 0;
+            } else {
+                width++;
+                loadingBar.style.width = width + "%";
+                loadingBar.innerHTML = width + "%";
+            }
+        }
+    }
 }

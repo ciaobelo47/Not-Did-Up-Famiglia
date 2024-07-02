8f4e3ad03ae8067790e4b5df5378925d57a97286
ciaobelo47 <milodipuorto@gmail.com>
2024-07-02 22:58:36 +0200

    - Started to add iframes tests

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,6 @@
 var TESTLI = document.getElementById('Documenti');
 
-
 TESTLI.children[0].addEventListener('click', ()=> {
     window.navigator.vibrate(1000);
     console.warn("BZZZZZZZZZZZZZZZ");
-})
+});
